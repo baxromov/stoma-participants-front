57817fd37e900e9a970ed3ab080ad83bb40420cd
baxromov <baxromov.shahzodbek@gmail.com>
2023-01-10 20:51:10 +0500

    add

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <>
         <Card id="wrapper">
-        <h1 style={{ textAlign: 'center', color: '#afc2db' }}>MicronLab</h1>
+        <h1 style={{ textAlign: 'center', color: '#277ae6' }}>MicronLab</h1>
           <h4 style={{color: "#fc0339", textAlign: 'center'}}><strong>В процессе регистрации вы должны быть подписаны на нашу страницу в Instagram.</strong></h4>
-          <h1 style={{textAlign:'center'}}><a href="#"><InstagramOutlined/></a></h1>
+          <h1 style={{textAlign:'center'}}><a href="#"><InstagramOutlined/>username</a></h1>
           <CreateForm />
         </Card>
 
