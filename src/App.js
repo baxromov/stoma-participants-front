e3ea07335aa47d7892def464f439a596f3acf342
baxromov1 <baxromov.shahzodbek@gmail.com>
2023-02-09 10:21:14 +0500

    Salom

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ function App() {
         </h1>
 
         <h1 style={{ textAlign: 'center', color: '#277ae6' }}>MicronLab</h1>
+        <h4 style={{ color: "#fc0339", textAlign: 'center' }}><strong>Ro'yxatdan o'tish jarayonida siz bizning Instagram sahifamizga obuna bo'lishingiz kerak.</strong></h4>
         <h4 style={{ color: "#fc0339", textAlign: 'center' }}><strong>В процессе регистрации вы должны быть подписаны на нашу страницу в Instagram.</strong></h4>
+        <h4 style={{ color: "#fc0339", textAlign: 'center' }}><strong>During the registration process, you must be subscribed to our Instagram page.</strong></h4>
         <h1 style={{ textAlign: 'center' }}><a href="https://www.instagram.com/micronlab_uz/" target="_blank"><InstagramOutlined />micronlab_uz</a></h1>
         <CreateForm />
       </Card>
